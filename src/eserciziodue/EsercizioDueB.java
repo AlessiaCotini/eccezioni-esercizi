704e71c2b906135c2989da5b41f57c1959c45335
AlessiaCotini <alessia.cotini@icloud.com>
2026-06-08 16:20:35 +0200

    esercizio due

--- src/eserciziodue/EsercizioDueB.java
+++ src/eserciziodue/EsercizioDueB.java
@@ -1,22 +1,24 @@
 package eserciziodue;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class EsercizioDue {
+public class EsercizioDueB {
     static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(java.util.Locale.US);
+        //virgola o punto RICORDARE!!!!!
         try{
             System.out.println("Calcoliamo il numero di chilometri percorsi per litro : ");
             System.out.println("Inserisci i chilometri che hai fatto : ");
-            int chilometri = scanner.nextInt();
+            double chilometri = scanner.nextDouble();
             System.out.println("Ora inserisci i litri di benzina consumati : ");
-            int litri = scanner.nextInt();
-            int risultato = chilometri/litri;
+            double litri = scanner.nextDouble();
+            double risultato = chilometri/litri;
+            if (litri == 0.0 | litri==0) throw new InputMismatchException();
             System.out.println("Con un litro hai percorso " + risultato + "km");
-        }catch (ArithmeticException ex){
-            System.out.println("Se non hai consumato litri di benzina non puoi aver percorso chilometri." );
-        }catch (Exception e) {
-            System.out.println("Non hai inserito un valore valido per il campo litri." );
+        } catch (Exception e) {
+            System.out.println("Il numero di litri deve essere definito.");
         }
     }
 }
