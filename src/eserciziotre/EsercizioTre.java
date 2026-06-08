b14bea8d40989b3d83956e35e2afe9b1ab1aa893
AlessiaCotini <alessia.cotini@icloud.com>
2026-06-08 16:49:47 +0200

    Update EsercizioTre.java

--- src/eserciziotre/EsercizioTre.java
+++ src/eserciziotre/EsercizioTre.java
@@ -5,12 +5,15 @@ public class EsercizioTre{
         ContoCorrente conto = new ContoCorrente("Ale",27.2);
         ContoOnLine contoOnLine = new ContoOnLine("RobertSmith",54879.24,220.0);
         try{
-            contoOnLine.preleva(60000.30);
-            contoOnLine.getSaldo();
+            //----------CONTO IN ROSSO---------------
             conto.preleva(28.0);
             conto.getSaldo();
+            //----------PRELIEVO NON DISPONIBILE-----
+            contoOnLine.preleva(60000.30);
+            contoOnLine.getSaldo();
+
         } catch (BancaException e) {
-            System.out.println("Il conto è in rosso, prova a ricaricare prima di voler prendere soldi che non hai!");
+            System.out.println("Prova a ricaricare, " + e.getMessage());
         }
 
     }
